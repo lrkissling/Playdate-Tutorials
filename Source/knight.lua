717fb81252661adaf4feab312d25bfb62071bb77
Lowell Kissling <lrkissling@gmail.com>
2023-01-03 23:45:01 -0800

    completed tutorial for OOP: https://www.youtube.com/watch?v=e9x6DsWk-IU

---
+++ Source/knight.lua
@@ -0,0 +1,24 @@
+import "player"
+
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+class('Knight').extends(Player)
+
+function Knight:init(x, y)
+	local knightImage = gfx.image.new("Images/knight")
+	Knight.super.init(self, x, y, knightImage)
+	self.moveSpeed = 5
+	self.projectileSpeed = 5
+end
+
+function Knight:ability()
+	Knight.super.ability(self)
+	
+	local function timerCallback()
+		self:shoot(self.x, self.y)
+	end
+	
+	pd.timer.performAfterDelay(250, timerCallback)
+	pd.timer.performAfterDelay(350, timerCallback)
+end
--- Source/main.lua
+++ Source/main.lua
@@ -1,12 +1,22 @@
 import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
-import "CoreLibs/tiemr"
+import "CoreLibs/timer"
+
+import "giant"
+import "knight"
+import "wizard"
 
 local pd <const> = playdate
-local gfx <const> = pd.grahics
+local gfx <const> = pd.graphics
 
 local function initialize()
+	local playerInstance = Wizard(200, 200)
+	playerInstance:add()
+	
+	local name = playerInstance.className --name == "Wizard"
+	local isGiant = playerInstance:isA(Giant) -- false
+	local isWizard = playerInstance:isA(Wizard) -- true
 end
 
 initialize()
